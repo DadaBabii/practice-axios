262e99decc34b9ee18fd9cadbb92004eed52fc27
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-02-17 14:01:30 +0000

    Deploying to gh-pages from @ DadaBabii/practice-axios@e337927b94a9102947c44c7f2f3692419e6ba443 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,23 +1,23 @@
-import{a as u}from"./assets/vendor-a2e8d7fa.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))c(o);new MutationObserver(o=>{for(const n of o)if(n.type==="childList")for(const s of n.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function r(o){const n={};return o.integrity&&(n.integrity=o.integrity),o.referrerpolicy&&(n.referrerPolicy=o.referrerpolicy),o.crossorigin==="use-credentials"?n.credentials="include":o.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function c(o){if(o.ep)return;o.ep=!0;const n=r(o);fetch(o.href,n)}})();function d({id:t,title:e,thumbnail:r,price:c}){return` <li class="item">
+import{a as d}from"./assets/vendor-a2e8d7fa.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))c(o);new MutationObserver(o=>{for(const n of o)if(n.type==="childList")for(const a of n.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&c(a)}).observe(document,{childList:!0,subtree:!0});function r(o){const n={};return o.integrity&&(n.integrity=o.integrity),o.referrerpolicy&&(n.referrerPolicy=o.referrerpolicy),o.crossorigin==="use-credentials"?n.credentials="include":o.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function c(o){if(o.ep)return;o.ep=!0;const n=r(o);fetch(o.href,n)}})();function u({id:e,title:t,thumbnail:r,price:c}){return` <li class="item" data-id="${e}">
   <div class ="thumb">
-  <h1> ${t} </h1>
+  <h1 > ${e} </h1>
         <img class="item-img" src="${r}" alt="picture" />
-        <p class="item-title">Tittle: ${e}</p>
+        <p class="item-title">Tittle: ${t}</p>
         <p class="item-price">Price: ${c}</p>
       </li>
-      </div>`}function m({id:t,description:e,title:r,price:c}){return` <li class="item">
+      </div>`}function m({id:e,description:t,title:r,price:c}){return` <li class="item">
   <div class ="thumb">
-  <h1> ${t} </h1>
-        <p class="item-desc"> Description: ${e}</p>
+  <h1> ${e} </h1>
+        <p class="item-desc"> Description: ${t}</p>
         <p class="item-title">Tittle: ${r}</p>
         <p class="item-price">Price: ${c}</p>
       </li>
-      </div>`}function p({id:t,title:e,thumbnail:r,price:c}){return`
+      </div>`}function p({id:e,title:t,thumbnail:r,price:c}){return`
  
   <div class="item thumb">
-  <h1> ${t} </h1>
+  <h1> ${e} </h1>
     <img class="item-img" src="${r}" alt="picture" />
-        <p class="item-title">${e}</p>
+        <p class="item-title">${t}</p>
         <p class="item-price">${c}</p>
-      </div>`}const a=u.create({baseURL:"https://dummyjson.com"});async function l(){const t=await a.get("/products?limit=15");return console.log(t.data),t.data}async function f(t){return(await a.get(`/products/${t}`)).data}async function g(t){return(await a.post("/products/add",t)).data}const i={productsList:document.querySelector("#allProducts"),getAllProdBtn:document.querySelector(".download-btn"),singleProductForm:document.querySelector("#singleProductForm"),singleProduct:document.querySelector("#singleProduct"),createProdForm:document.querySelector("#createNewProductForm"),creatingProduct:document.querySelector("#newProductSection")};async function P(){const e=(await l()).products.map(d).join("");i.productsList.insertAdjacentHTML("beforeend",e)}i.getAllProdBtn.addEventListener("click",y);function y(t){t.preventDefault(),l(),P()}i.singleProductForm.addEventListener("submit",h);async function h(t){t.preventDefault();const e=t.target.elements.id.value.trim();try{const r=await f(e),c=p(r);i.singleProduct.innerHTML=c}catch(r){console.log(r.message)}t.target.reset()}i.createProdForm.addEventListener("submit",v);async function v(t){t.preventDefault();const e={title:t.target.elements.title.value.trim(),description:t.target.elements.description.value.trim(),price:t.target.elements.price.value.trim()};try{const r=await g(e),c=m(r);i.creatingProduct.insertAdjacentHTML("afterbegin",c)}catch(r){console.log(r)}t.target.reset()}
+      </div>`}const i=d.create({baseURL:"https://dummyjson.com"});async function l(){const e=await i.get("/products?limit=15");return console.log(e.data),e.data}async function f(e){return(await i.get(`/products/${e}`)).data}async function g(e){try{return(await i.post("/products/add",e)).data}catch(t){console.log(t)}}async function y(e){try{const t=await i.delete(`/products/${e}`);return console.log(t.data),t.data}catch(t){console.log(t)}}const s={productsList:document.querySelector("#allProducts"),getAllProdBtn:document.querySelector(".download-btn"),singleProductForm:document.querySelector("#singleProductForm"),singleProduct:document.querySelector("#singleProduct"),createProdForm:document.querySelector("#createNewProductForm"),creatingProduct:document.querySelector("#newProductSection"),deletionProductForm:document.querySelector("#deletionProductForm")};async function P(){const t=(await l()).products.map(u).join("");s.productsList.insertAdjacentHTML("beforeend",t)}s.getAllProdBtn.addEventListener("click",v);function v(e){e.preventDefault(),l(),P()}s.singleProductForm.addEventListener("submit",h);async function h(e){e.preventDefault();const t=e.target.elements.id.value.trim();try{const r=await f(t),c=p(r);s.singleProduct.innerHTML=c}catch(r){console.log(r.message)}e.target.reset()}s.createProdForm.addEventListener("submit",L);async function L(e){e.preventDefault();const t={title:e.target.elements.title.value.trim(),description:e.target.elements.description.value.trim(),price:e.target.elements.price.value.trim()};try{const r=await g(t),c=m(r);s.creatingProduct.insertAdjacentHTML("afterbegin",c)}catch(r){console.log(r)}e.target.reset()}s.deletionProductForm.addEventListener("submit",$);async function $(e){e.preventDefault();const t=e.target.elements.deletionId.value.trim();await y(t),document.querySelector(`[data-id="${t}"]`).remove(),e.target.reset()}
 //# sourceMappingURL=commonHelpers.js.map
